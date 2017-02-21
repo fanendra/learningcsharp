1965131a1b48e17d1dca40284286d193c3559914
fanendra <fanendra.ganti@gmail.com>
2017-02-20 16:50:14 -0800

    Adding Iinq to object class.

---
+++ consoleApp/ConsoleApplication1/ConsoleApplication1/LinqToObjects.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ConsoleApplication1
+{
+    public static class LinqToObjects
+    {
+        static LinqToObjects()
+        {
+            //Static constructor , only once executed.
+            linqFun1();
+        }
+
+        // You can't access it outside.
+       static void linqFun1()
+        {
+            List<int> listNumbs = new List<int>();
+            listNumbs.AddRange(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+
+            List<int> evennumbers = listNumbs.FindAll(e => e % 2 == 0);
+
+            // Use of Extention Methods.
+            listNumbs.printEachNumber();
+
+            evennumbers.printEachNumber();
+
+        }
+
+        // Can be accessed out side.
+        public static void sayHello()
+        {
+            WriteLine("hi from LinqToObjects1  !!!");
+        }
+
+        /// <summary>
+        /// Print all the numbers in the List
+        /// </summary>
+        /// <param name="numbers">List of numbers</param>
+        private static void printEachNumber(this List<int> numbers)
+        {
+            foreach (int num in numbers)
+            {
+                WriteLine(num);
+            }
+        }
+
+    }
+}
--- consoleApp/ConsoleApplication1/ConsoleApplication1/Program.cs
+++ consoleApp/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -3,13 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
+
 
 namespace ConsoleApplication1
 {
-    class Program
+    enum Colorcoding
     {
+        pink = 20,
+        red= 30, 
+        blue = 40
+    }
+
+    public class Program 
+    {
+
         static void Main(string[] args)
         {
+            ForegroundColor = ConsoleColor.Yellow;
+            BackgroundColor = ConsoleColor.Black;
+            WriteLine("Lets do it, this time serious");
+            WriteLine("");
+            WriteLine("######       # ");
+            WriteLine("#           # #" );
+            WriteLine("####       #####");
+            WriteLine("#         #     #");
+            WriteLine("#        #       #");
+
+            WriteLine("--------------------------------------");
+            WriteLine();
+
+            LinqToObjects.sayHello();
+           
+
+
+
+
+
+            ReadLine();
         }
     }
 }
