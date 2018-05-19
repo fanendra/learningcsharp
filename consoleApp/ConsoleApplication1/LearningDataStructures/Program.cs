71e7729a81b1a730ad6726f46f6e6d8c1c91a073
fanendra <fanendra.ganti@gmail.com>
2018-05-19 17:53:37 -0500

    linkedlist learning changes

--- consoleApp/ConsoleApplication1/LearningDataStructures/Program.cs
+++ consoleApp/ConsoleApplication1/LearningDataStructures/Program.cs
@@ -10,7 +10,30 @@ namespace LearningDataStructures
     {
         static void Main(string[] args)
         {
+            SingleLinkedList list = new SingleLinkedList();
+            list.InsertNewNode(1);
+            list.InsertNewNode(2);
+            list.InsertNewNode(3);
+            list.InsertNewNode(4);
+            list.InsertNewNode(5);
+            list.InsertNewNode(6);
+            list.InsertNewNode(7);
+            list.DisplayList();
+            //list.CountNode();
+            //list.Search(10);
+            list.InsertNodeAtBegining(11);
+            list.DisplayList();
 
+            list.insertNodeAtEnd(12);
+            list.DisplayList();
+
+
+            list.insertAtNthPosition(13,5);
+            list.DisplayList();
+
+            list.ReverseList();
+            list.DisplayList();
+            Console.Read();
         }
     }
 }
---
+++ consoleApp/ConsoleApplication1/LearningDataStructures/SingleLinkedList.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningDataStructures
+{
+    class SingleLinkedList
+    {
+        private Node start;
+
+        public SingleLinkedList()
+        {
+            start = null;
+        }
+
+        public void InsertNewNode(int info)
+        {
+            Node temp = new Node(info);
+
+            if (start == null)
+            {
+                start = temp;
+            }
+            else
+            {
+                Node p = start;
+                while(p.link !=null)
+                {
+                    p = p.link;
+                }
+                if (p.link == null)
+                    p.link = temp;
+            }
+        }
+
+        public void InsertNodeAtBegining(int info)
+        {
+            Node temp = new Node(info);
+            if (start != null)
+            {
+                temp.link = start;
+                start = temp;
+            }            
+        }
+
+        public void insertNodeAtEnd(int info)
+        {
+            Node temp = new Node(info);
+            Node p = start;
+            while (p.link != null)
+            {
+                p = p.link;
+            }
+            p.link = temp;
+        }
+
+        public void insertAtNthPosition(int info, int k)
+        {
+            Node temp = new Node(info);
+            Node p = start;
+            for (int i = 1; i<k && p.link!= null; i++)
+            {
+                p = p.link;  
+            }
+
+            temp.link = p.link;
+            p.link = temp ;
+                    
+        }
+
+
+        public void ReverseList()
+        {
+            Node prev = null;
+            Node p = null;
+            Node next = null;
+            p = start;
+            while (p != null)
+            {
+                next = p.link;
+                p.link = prev;
+                prev = p;
+                p = next;
+            }
+            start = prev;
+        }
+
+        public void DisplayList()
+        {
+            Node p; 
+
+            if(start == null)
+            {
+                Console.WriteLine("List is Empty");
+                return;
+            }
+
+            p = start;
+
+            Console.Write("List Is :");
+
+            while (p != null)
+            {
+                Console.Write(p.info + " ");
+                p = p.link;
+            }
+            Console.WriteLine();
+        }
+
+        public void CountNode()
+        {
+            int n = 0;
+            Node p = start;
+
+            while (p !=null)
+            {
+                n++;
+                p = p.link;
+            }
+
+            Console.WriteLine("Number of the nodes in the List are: " + n);
+        }
+
+        public void Search(int x)
+        {
+            int position = 1;
+
+            Node p = start;
+
+            while (p != null)
+            {
+                if (p.info == x)
+                {
+                  break;
+                }
+                position++;
+                p = p.link;
+            }
+
+            if(p == null)
+            {
+                Console.WriteLine("The Element is not found in the list.");
+            }
+            else
+            {
+                Console.WriteLine("Element Found at " + position + " position in the list");
+            }
+
+        }
+    }
+}
